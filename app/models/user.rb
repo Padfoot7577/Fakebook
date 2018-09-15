6e4b23c92e275121e28f4a33b8dd9cd098bd1ead
Yanlin Li <orthrus@mit.edu>
2018-09-15 15:03:09 -0400

    Added User.

---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                            :integer          not null, primary key
+#  email                         :string           default(""), not null
+#  name                          :string           default(""), not null
+#  created_at                    :datetime         not null
+#  updated_at                    :datetime         not null
+#  password_digest               :string
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+
+class User < ActiveRecord::Base
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :name,  length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  before_save :format_email
+
+  def format_email
+    self.email = email.downcase
+  end
+
+end
