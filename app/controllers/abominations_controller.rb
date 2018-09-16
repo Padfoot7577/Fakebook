66587916357aadc71f89a8a2e0dc174344a44e50
Yanlin Li <orthrus@mit.edu>
2018-09-16 00:34:19 -0400

    Added some controller and view stuff for abominations.

--- app/controllers/abominations_controller.rb
+++ app/controllers/abominations_controller.rb
@@ -1,4 +1,22 @@
 class AbominationsController < ApplicationController
   def new
+    if !logged_in?
+      return redirect_to '/'
+    end
+    @abomination = Abomination.new
+  end
+
+  def create
+    @abomination = Abomination.new(params.require(:abomination).permit(:name, :description, :url))
+    if @abomination.save
+      flash[:success] = "Haters gotta hate."
+      redirect_to @abomination
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @abomination = Abomination.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
 
   get  '/signup' => 'users#new'
   post '/signup' => 'users#create'
+
   resources :users
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :abominations
 end
